45af2686bb03c823611d7a4f0e02e574e2118643
vainiovesa <vesa.vainio@helsinki.fi>
2026-02-03 11:49:21 +0200

    Add web UI

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from search import search
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    query_limit = 10
+
+    query = None
+    result = []
+    if request.method == "POST":
+        query = request.form["query"]
+        result = search(query, query_limit)
+
+    return render_template("index.html", query=query, result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
